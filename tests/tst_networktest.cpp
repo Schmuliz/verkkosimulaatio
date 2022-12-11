c566f5fd1efe1d1a4aa72bc08a649a3999042808
Tuomo Nappa <tuomo.nappa@gmail.com>
2022-12-11 20:35:02 +0200

    Add test for packet age

--- tests/tst_networktest.cpp
+++ tests/tst_networktest.cpp
@@ -33,6 +33,29 @@ private slots:
         //QCOMPARE(1, 1); // compare two values
     }
 
+    void packetTimingTest()
+    {
+        Network* network2 = new Network();
+        EndHost* a = new EndHost(1, {3,0,256,2});
+        EndHost* b = new EndHost(2, {3,0,256,1});
+        network2->addNode(a);
+        network2->addNode(b);
+        network2->addLink(1,2,500,50);
+        network2->initializeRoutingTables();
+        Packet* p = new Packet(1, 2, 256);
+        b->processPacket(p); // manually send packet to b
+        int ticker = 0;
+        while (a->getLastPacketAge() == 0) {
+            network2->runOneTick();
+            ticker++;
+        }
+        int packetAge = a->getLastPacketAge();
+        qDebug() << "Current tick:" << ticker;
+        qDebug() << "Packet age:" << packetAge;
+        delete network2;
+        QVERIFY(ticker == packetAge);
+    }
+
 
     void cleanupTestCase()
     {
